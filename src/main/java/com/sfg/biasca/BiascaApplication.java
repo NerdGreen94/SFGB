dfc3d89d98ed724c32ab0c876de678e869980c41
NerdGreen94 <jpallares44@gmail.com>
2026-03-24 16:48:45 +0100

    Initial commit

---
+++ src/main/java/com/sfg/biasca/BiascaApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.biasca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiascaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiascaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfg/biasca/BiascaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.biasca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiascaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
