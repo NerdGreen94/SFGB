bd7c5c2f19f7b82da91e03ec010d6d6e517de347
NerdGreen94 <jpallares44@gmail.com>
2026-03-24 17:23:25 +0100

    sistemazione progetto

---
+++ src/main/java/com/sfg/biasca/HomeController.java
@@ -0,0 +1,13 @@
+package com.sfg.biasca;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
