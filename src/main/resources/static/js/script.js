90c53faa9777ccec6cb499c02dc1ae2f723b66cf
NerdGreen94 <jpallares44@gmail.com>
2026-03-31 03:32:54 +0200

    prima struttura sito

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,11 @@
+<script>
+    // CAMBIO NAVBAR ALLO SCROLL
+    window.addEventListener("scroll", function () {
+      let navbar = document.querySelector(".navbar");
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+</script>
